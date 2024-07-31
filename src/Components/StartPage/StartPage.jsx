757d488bc954676a9f056cd7cb9fdd58b1e2909d
K7413dS433d <khaledsaied2002@gmail.com>
2024-08-01 02:48:25 +0300

    add helmet in start page

--- src/Components/StartPage/StartPage.jsx
+++ src/Components/StartPage/StartPage.jsx
@@ -1,8 +1,12 @@
 import avatar from "./../../assets/images/avataaars.svg";
+import { Helmet } from "react-helmet";
 
 function StartPage() {
   return (
     <section className=" bg-[#1abc9c] text-white flex justify-center  items-center flex-col py-20 mt-[6.5rem]">
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
       <img src={avatar} alt="avatar image" className=" w-[250px] mb-6" />
       <h1 className=" font-bold text-2xl md:text-4xl mb-5">START FRAMEWORK</h1>
       <div className="flex justify-between items-center w-[250px] mb-5">
